4798ca613459f0b0fed85771971455f75235565d
Udit Doshi <47755930+uditdoshi@users.noreply.github.com>
2026-08-04 23:14:49 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'easyinvoice-final-phone-fix-2';
+const CACHE = 'easyinvoice-final-phone-fix-3';
 const ASSETS = [
   './', './index.html', './assets/style.css', './assets/app.js',
   './assets/database.js', './config.js', './data/data.js',
